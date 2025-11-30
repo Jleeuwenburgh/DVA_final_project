ae0be4acece6c3772823a851e74b36be30540f23
Jleeuwenburgh <joep.leeuwenburgh@gmail.com>
2025-11-30 20:10:39 +0100

    generated ydata profile reports for all reccomended datasets

---
+++ generate_profiles.py
@@ -0,0 +1,20 @@
+'''
+Generates data profiles using ydata profiling.
+For the datasets:
+- heart disease
+- titanic 
+- housing
+'''
+
+import ydata_profiling as yp
+import pandas as pd
+
+folder = 'data/'
+output_folder = 'profile_reports/'
+
+datasets = ['heart.csv', 'titanic-dataset.csv', 'housing.csv']
+
+for dataset in datasets:
+    df = pd.read_csv(folder + dataset)
+    profile = yp.ProfileReport(df, title=f"{dataset.split('.')[0].capitalize()} Data Profile", explorative=True)
+    profile.to_file(output_folder + f"{dataset.split('.')[0]}_data_profile.html")
